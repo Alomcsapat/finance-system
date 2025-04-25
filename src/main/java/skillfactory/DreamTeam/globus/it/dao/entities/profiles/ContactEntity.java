5323ea6591ae4f7ee41700d54b538ab9c49c8eb1
JavaMan <Kechin.d@gmail.com>
2025-04-25 16:23:15 +0300

    add contact entity

--- src/main/java/skillfactory/DreamTeam/globus/it/dao/entities/profiles/ContactEntity.java
+++ src/main/java/skillfactory/DreamTeam/globus/it/dao/entities/profiles/ContactEntity.java
@@ -1,11 +1,13 @@
 package skillfactory.DreamTeam.globus.it.dao.entities.profiles;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
+@Table(name = "contacts")
 @SuperBuilder
 @NoArgsConstructor
 public abstract class ContactEntity extends ProfileEntity {
