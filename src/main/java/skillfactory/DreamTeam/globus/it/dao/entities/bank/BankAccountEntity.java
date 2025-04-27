046fad8873fe14d838b8ad6e702131d69711ad7d
izykov <izykov@ds-in.net>
2025-04-27 15:02:57 +0200

    bank account entity changed

--- src/main/java/skillfactory/DreamTeam/globus/it/dao/entities/bank/BankAccountEntity.java
+++ src/main/java/skillfactory/DreamTeam/globus/it/dao/entities/bank/BankAccountEntity.java
@@ -1,16 +1,20 @@
 package skillfactory.DreamTeam.globus.it.dao.entities.bank;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import skillfactory.DreamTeam.globus.it.dao.entities.profiles.ProfileEntity;
 import skillfactory.DreamTeam.globus.it.dao.entities.GeneralEntity;
 
-@Data
+@Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,7 +26,8 @@ public class BankAccountEntity extends GeneralEntity {
     @ManyToOne
     private BankEntity bank;
     private BigDecimal balance;
-    private String accountNumber;
+    @Builder.Default
+    private String accountNumber = UUID.randomUUID().toString();
     private String title;
     //private List<OperationEntity> logs;
 }
